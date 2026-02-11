1fbbfd899df0485086800367a16196aa62208b64
arthxvz <arthxvz@gmail.com>
2026-02-10 21:05:36 -0300

    a

--- js/app.js
+++ js/app.js
@@ -9,3 +9,20 @@ function login() {
     window.location.href = "usuario.html";
   }
 }
+function login() {
+  const id = document.getElementById("idMaquina").value.trim();
+  const senha = document.getElementById("senha").value.trim();
+
+  // validação mínima
+  if (!id || !senha) {
+    alert("Preencha ID e senha");
+    return;
+  }
+
+  // identifica perfil
+  if (id.toUpperCase().startsWith("TEC")) {
+    window.location.href = "tecnico.html";
+  } else {
+    window.location.href = "usuario.html";
+  }
+}
